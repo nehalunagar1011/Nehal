6ca43dd7cd99f8ace2c6bfd8052980ae8d310ba9
nehal <nehal.unagar@contcentric.com>
2021-05-06 14:22:59 +0530

    Fifth May

--- FifthMay/src/TempleD.java
+++ FifthMay/src/TempleD.java
@@ -4,26 +4,17 @@ import java.util.*;
 public class TempleD {
 
 		public static void main(String[] args) {
-			float total_amount=0,amt=2;
-			
-			Scanner in=new Scanner(System.in);
-			System.out.println("Enter the number of visited temples: ");
-			int no_of_temple = in.nextInt();
-			System.out.println("Enter donation amount: ");
-			int da=in.nextInt();
-			in.close();
-			
-			for(int i=1;i<=no_of_temple;i++)
-			{
-				total_amount=(2*total_amount)+da;
-				amt=amt*2;
-				
-			}
-			
-			float final_initial_amount= total_amount /(amt/2); 	
-			System.out.println("The initial amount is "+final_initial_amount);
-
+			fdouble amount = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of temples visited: ");
+		int temple = sc.nextInt();
+		System.out.print("Enter Donation amount: ");
+		int don = sc.nextInt();
+		sc.close();
+		
+		double rem = Math.pow(2, temple);
+		amount = (((rem-1)*don)/rem);
+		System.out.println("The Intial Amount Is: "+amount);
 		}
-
-	}
 }
